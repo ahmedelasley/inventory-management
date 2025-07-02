d5895d5cf9f646ee8bc3e2dd3ed8dc561f6cd09c
ahmedelasley <ahmedelasley@gmail.com>
2025-07-02 17:14:39 +0300

    create scopes to split select type (menu - stock)

---
+++ app/Models/Scopes/StockTypeScope.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class StockTypeScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        $builder->whereHas('category', function ($query) {
+            $query->where('type', 0); // قسم نوعه "منتجات"
+        });
+    }
+}
