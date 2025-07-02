64e075b9f9cbc7141e3fc38295da66852ac3601b
ahmedelasley <ahmedelasley@gmail.com>
2025-07-02 16:42:17 +0300

    Create and Edit Files Export

--- app/Exports/CategoriesExport.php
+++ app/Exports/CategoriesExport.php
@@ -3,37 +3,49 @@
 namespace App\Exports;
 
 use App\Models\Category;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-
-class CategoriesExport implements FromCollection, WithHeadings
+class CategoriesExport implements FromQuery, WithHeadings, WithMapping, ShouldQueue
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    use Exportable;
+
+    protected $id = 1;
+
+    public function query()
     {
-        return Category::all();
+        return Category::with(['parent', 'creator', 'updater']);
     }
-
-    /**
-     * Write code on Method
-     *
-     * @return response()
-     */
-
      public function headings(): array
      {
          return [
-            'id',
-            'name',
-            'description',
-            'parent_id',
-            'created_id',
-            'updated_id',
-            'created_at',
-            'updated_at',
+            '#',
+            'Name',
+            'Description',
+            'Parent',
+            'Type',
+            'Creator',
+            'Editor',
+            'Created at',
+            'Updated At',
          ];
      }
+     
+    public function map($category): array
+    {
+        return [
+            $this->id++,
+            $category->name,
+            $category->description,
+            $category->parent?->name,
+            $category->type == 0 ? 'stock' : 'menu',
+            $category->creator?->name,
+            $category->updater?->name,
+            $category->created_at,
+            $category->updated_at,
+        ];
+    }
 }
---
+++ app/Exports/InventoryKitchenExport.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\KitchenStock;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+
+class InventoryKitchenExport implements FromQuery, WithHeadings, WithMapping, ShouldQueue
+{
+    use Exportable;
+
+    protected $sort = 1;
+    protected $id;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+
+    public function query()
+    {
+
+        $data = KitchenStock::with(['product', 'kitchen'])->ofKitchen($this->id)->latest();
+
+        return $data;
+
+
+    }
+
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+
+     public function headings(): array
+     {
+         return [
+            '#',
+            'Name',
+            'SKU',
+            'Live Qty Stock',
+            'Intgredient',
+            'Cost',
+            'Total',
+         ];
+     }
+
+        public function map($data): array
+    {
+        return [
+            $this->sort++,
+            $data->product->name,
+            $data->product->sku,
+            $data->quantity . ' ' . $data->product->storge_unit ,
+            $data->quantity * $data->product->storage_to_intgredient . $data->product->intgredtiant_unit ,
+            $data->cost,
+            ($data->quantity == 0 ? '0' : $data->quantity * $data->cost ),
+        ];
+    }
+}
